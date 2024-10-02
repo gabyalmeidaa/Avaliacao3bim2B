196dc1ecc43c42516354d7a31161b0d99c555dd4
gabriely <gabriely.as04@gmail.com>
2024-10-01 21:12:05 -0300

    versionamneto

---
+++ gaby.py
@@ -0,0 +1,16 @@
+sala = int(input("escreva a quantidade de pessoas para sua reuniao:"))
+
+if sala <3:
+  print ("utilizar sala pequena")
+
+elif sala <10:
+  print("utilizar sala media")
+
+else:
+  print("utilizar sala grande")
+
+
+
+
+
+
